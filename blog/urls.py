aff8cd9210419f5322647b4602d93af8d462ee9e
testDrive64 <com9on@me.com>
2016-06-14 13:25:49 +0200

    correct the urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': reverse_lazy('post_list')}, name='logout'),
 ]
--- freeSpeech/urls.py
+++ freeSpeech/urls.py
@@ -20,6 +20,4 @@ from django.core.urlresolvers import reverse_lazy
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='freeSpeech_login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': reverse_lazy('marcador_bookmark_list')}, name='freeSpeech_logout'),
 ]
