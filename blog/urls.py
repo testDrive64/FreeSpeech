f0ebab24b09eb50d3943763ef606b34658cd3133
testDrive64 <com9on@me.com>
2016-06-14 22:37:52 +0200

    Add a part for the login

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
-    #url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
-    #url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'post_list'}, name='logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
